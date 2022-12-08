93db53bca1e34d01f8712d2ce7c7083b44130a8e
Romain Landolfini <romain.landolfini@gmail.com>
2022-12-08 12:12:11 +0000

    change guess title display

--- src/Components/PokemonTitle.js
+++ src/Components/PokemonTitle.js
@@ -5,7 +5,7 @@ const PokemonTitle = ({pokeName, pokemonID, pokedexMode}) => {
 
     return(
         <>
-        {pokedexMode ? <h2>#{pokemonID} {pokeName}</h2> : <h2>????????????</h2>}
+        {pokedexMode ? <h2>#{pokemonID} {pokeName}</h2> : <h2>#?? ????????</h2>}
         </>  
     )
 }
